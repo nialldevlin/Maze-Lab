4aa58012072a09724d04fed763afb93a0dbbbcb5
nialldevlin <32718927+nialldevlin@users.noreply.github.com>
2022-02-05 01:02:02 -0700

    Update Pathfinder.cpp

--- Pathfinder.cpp
+++ Pathfinder.cpp
@@ -91,7 +91,7 @@ vector<string> Pathfinder::solveMaze() {
 		return solved;
 	}
 }
-
+/*
 //Private functions
 bool Pathfinder::up(Coord * pos) {
 	if (pos->getz() - 1 >= 0 && maze[pos->getz() - 1][pos->gety()][pos->getx()] == true) {
@@ -140,3 +140,4 @@ bool Pathfinder::right(Coord * pos) {
 	}
 	return false;
 }
+*/
